61a5e6bdaf67579197a4a64c0180f8b40cec5f33
bytecoder5 <andriiluzhanskyy@gmail.com>
2024-07-14 10:13:34 +0300

    init

---
+++ src/components/CompanyLogos.jsx
@@ -0,0 +1,14 @@
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ({ className }) => {
+  return (
+    <div className={className}>
+      <h5 className="tagline mb-6 text-center text-n-1/50">
+        WayCode
+      </h5>
+      
+    </div>
+  );
+};
+
+export default CompanyLogos;
---
+++ src/hello.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className='header'>
+            <span className='title'>Word Hunt</span>
+        </div>
+    )
+}
+
+export default Header
